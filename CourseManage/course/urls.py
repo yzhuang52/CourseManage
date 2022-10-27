db2f731f13b56db6bcb1ff05425b3213155b4c8f
littlekimber <zhyan0096@gmail.com>
2022-10-26 23:07:41 -0500

    basic crud operation finish

--- CourseManage/course/urls.py
+++ CourseManage/course/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import CourseList, AddCourse
-from course import views
+from .views import get, get_all_course, post, delete, update
 
 urlpatterns = [
-    path("course-list/", views.CourseList.as_view()),
-    path("course/", AddCourse),
+    path("course/", get_all_course, name="courses"),
+    path("course/add/", post, name="add-course"),
+    path("course/<str:name>/", get, name="course"),
+    path("course/delete/<str:name>/", delete, name="delete-course"),
+    path("course/update/<str:name>/", update, name="update-course")
 ]
--- CourseManage/course/views.py
+++ CourseManage/course/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,16 +6,29 @@ from .serializers import CourseSerializer
 from .models import Course
 from rest_framework.decorators import api_view
 from rest_framework import status
-class CourseList(APIView):
-    def get(self, request, format=None):
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data)
 
-@api_view(['POST'])
-def AddCourse(request):
+def get_course(name):
+    try:
+        return Course.objects.get(name=name)
+    except:
+        raise Http404
+@api_view(["GET"])
+def get_all_course(request):
+    courses = Course.objects.all()
+    serializer = CourseSerializer(courses, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get(request, name):
+    course = get_course(name)
+    serializer = CourseSerializer(course)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+def post(request):
     data = request.data
-    print(data)
     serializer = CourseSerializer(data=data)
     if serializer.is_valid():
         serializer.save()
@@ -22,3 +36,17 @@ def AddCourse(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(["DELETE"])
+def delete(request, name):
+    course = get_course(name)
+    course.delete()
+    return Response({"message": "delete successfully"}, status=status.HTTP_200_OK)
+
+
+@api_view(["PUT"])
+def update(request, name):
+    course = get_course(name)
+    course.name = request.data["name"]
+    course.credit = request.data["credit"]
+    serializer = CourseSerializer(course)
+    return Response(serializer.data, status=status.HTTP_200_OK)
